d23a7c6a3e995bcc496537bb5a538fece99410e9
Basudev <samantabasu.2001@gmail.com>
2024-12-02 17:35:46 +0530

    payment details api completed

---
+++ src/main/java/com/basu/trading/repository/PaymentDetailsRepository.java
@@ -0,0 +1,8 @@
+package com.basu.trading.repository;
+
+import com.basu.trading.model.PaymentDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentDetailsRepository extends JpaRepository<PaymentDetails, Long> {
+    PaymentDetails findByUserId(Long userId);
+}
---
+++ src/main/java/com/basu/trading/service/PaymentDetailsService.java
@@ -0,0 +1,14 @@
+package com.basu.trading.service;
+
+import com.basu.trading.model.PaymentDetails;
+import com.basu.trading.model.User;
+
+public interface PaymentDetailsService {
+    public PaymentDetails addPaymentDetails(String accountNumber,
+                                            String accountHolderName,
+                                            String ifsc,
+                                            String bankName,
+                                            User user);
+
+    public PaymentDetails getUsersPaymentDetails(User user);
+}
