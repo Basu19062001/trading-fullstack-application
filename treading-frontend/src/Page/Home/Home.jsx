220d09ca76dacdc20a77e8ba245eceafe58d7258
Basudev <samantabasu.2001@gmail.com>
2024-12-08 19:06:46 +0530

    Asset Table added in home

--- treading-frontend/src/Page/Home/Home.jsx
+++ treading-frontend/src/Page/Home/Home.jsx
@@ -1,8 +1,53 @@
+ import { Button } from '@/components/ui/button'
 import React from 'react'
+import AssetTable from './AssetTable'
 
 function Home() {
+    const [category, setCategory] = React.useState("all")
+
+    const handleCategory = (value) => {
+        setCategory(value)
+    }
+
   return (
-    <div>Home</div>
+    <div className='relative'>
+        <div className='lg:flex'>
+            <div className='lg:w-[50%] lg:border-r'>
+                <div className='p-3 flex items-center gap-4'>
+                    <Button 
+                    className="rounded-full"
+                    variant={category == "all" ? "default" : "outline"}
+                    onClick={()=> handleCategory("all")}>
+                        All
+                    </Button>
+
+                    <Button 
+                    className="rounded-full"
+                    variant={category == "top50" ? "default" : "outline"}
+                    onClick={()=> handleCategory("top50")}>
+                        Top 50
+                    </Button>
+
+                    <Button 
+                    className="rounded-full"
+                    variant={category == "topGainers" ? "default" : "outline"}
+                    onClick={()=> handleCategory("topGainers")}>
+                        Top Gainers
+                    </Button>
+
+                    <Button 
+                    className="rounded-full"
+                    variant={category == "topLosers" ? "default" : "outline"}
+                    onClick={()=> handleCategory("topLosers")}>
+                        Top Losers
+                    </Button>
+                </div>
+
+                <AssetTable/>
+                
+            </div>
+        </div>
+    </div>
   )
 }
 
