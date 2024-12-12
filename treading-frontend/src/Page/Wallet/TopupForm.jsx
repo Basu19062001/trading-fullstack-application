3c075f2014136b7a53cd58c445449e57ef31588d
Basudev <samantabasu.2001@gmail.com>
2024-12-12 21:26:12 +0530

    wallet added

---
+++ treading-frontend/src/Page/Wallet/TopupForm.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const TopupForm = () => {
+  return <div>TopupForm</div>;
+};
+
+export default TopupForm;
--- treading-frontend/src/Page/Wallet/Wallet.jsx
+++ treading-frontend/src/Page/Wallet/Wallet.jsx
@@ -1,7 +1,112 @@
+/* eslint-disable no-unused-vars */
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { ReloadIcon } from "@radix-ui/react-icons";
+import {
+  ArrowDown,
+  ArrowLeftCircle,
+  Banknote,
+  BanknoteIcon,
+  CopyIcon,
+  CreditCard,
+  DollarSign,
+  Shuffle,
+  UploadIcon,
+  WalletIcon,
+} from "lucide-react";
 import React from "react";
+import TopupForm from "./TopupForm";
+import WithdrawalForm from "../Withdrawal/WithdrawalForm";
+import TransferForm from "./TransferForm";
 
 const Wallet = () => {
-  return <div>Wallet</div>;
+  return (
+    <div className="flex flex-col items-center">
+      <div className="pt-10 w-full lg:w-[60%]">
+        <Card>
+          <CardHeader className="pb-9">
+            <div className="flex justify-between items-center">
+              <div className="flex items-center gap-5">
+                <WalletIcon size={30} className="" />
+                <div>
+                  <CardTitle className="text-2xl">My Wallet</CardTitle>
+                  <div className="flex items-center gap-2">
+                    <p className="text-gray-200 text-sm">#A475Ed</p>
+                    <CopyIcon
+                      size={12}
+                      className="cursor-pointer hover:text-slate-300"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div>
+                <ReloadIcon className="w-6 h-6 cursor-pointer hover:text-gray-400" />
+              </div>
+            </div>
+          </CardHeader>
+          <CardContent>
+            <div className="flex items-center">
+              <DollarSign />
+              <span className="text-2xl font-semibold">20000</span>
+            </div>
+
+            <div className="flex gap-7 mt-5">
+              <Dialog>
+                <DialogTrigger>
+                  <div className="h-24 w-24 hover:text-gray-400 cursor-pointer flex flex-col items-center justify-center rounded-md shadow-slate-800 shadow-md">
+                    <UploadIcon />
+                    <span className="text-sm mt-2">Add Money</span>
+                  </div>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>Top Up Your Wallet</DialogTitle>
+                  </DialogHeader>
+                  <TopupForm />
+                </DialogContent>
+              </Dialog>
+
+              <Dialog>
+                <DialogTrigger>
+                  <div className="h-24 w-24 hover:text-gray-400 cursor-pointer flex flex-col items-center justify-center rounded-md shadow-slate-800 shadow-md">
+                    <Banknote />
+                    <span className="text-sm mt-2">Withdrawal</span>
+                  </div>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>Request Withdrawal</DialogTitle>
+                  </DialogHeader>
+                  <WithdrawalForm />
+                </DialogContent>
+              </Dialog>
+
+              <Dialog>
+                <DialogTrigger>
+                  <div className="h-24 w-24 hover:text-gray-400 cursor-pointer flex flex-col items-center justify-center rounded-md shadow-slate-800 shadow-md">
+                    <Shuffle />
+                    <span className="text-sm mt-2">Transfer</span>
+                  </div>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>Transfer to other wallet</DialogTitle>
+                  </DialogHeader>
+                  <TransferForm />
+                </DialogContent>
+              </Dialog>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default Wallet;
