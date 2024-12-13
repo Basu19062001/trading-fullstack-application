3e44127858f591cde51f21edff967a542eb1ca3f
Basudev <samantabasu.2001@gmail.com>
2024-12-13 23:52:49 +0530

    Withdrawal form completed

--- treading-frontend/src/Page/Withdrawal/WithdrawalForm.jsx
+++ treading-frontend/src/Page/Withdrawal/WithdrawalForm.jsx
@@ -1,8 +1,62 @@
 /* eslint-disable no-unused-vars */
+import { Button } from "@/components/ui/button";
+import { DialogClose } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
 import React from "react";
 
 const WithdrawalForm = () => {
-  return <div>WithdrawalForm</div>;
+  const [amount, setAmount] = React.useState("");
+
+  const handleChange = (e) => {
+    setAmount(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    console.log(amount);
+  };
+  return (
+    <div className="pt-10 space-y-5">
+      <div className="flex justify-between items-center rounded-md bg-slate-900 text-xl font-bold px-5 py-4">
+        <p>Available balance</p>
+        <p>$9999</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <h1>Enter Withdrawal Amount</h1>
+        <div className="flex  items-center justify-center">
+          <Input
+            onChange={handleChange}
+            value={amount}
+            className="withdrawalInput py-7 border-none outline-none focus:outline-none px-0 text-2xl text-center"
+            placeholder="$9999"
+            type="number"
+          />
+        </div>
+      </div>
+      <div>
+        <p className="pb-2">Transfer to</p>
+        <div className="flex items-center gap-5 border px-5 py-2 rounded-md">
+          <img
+            className="h-8 w-8 "
+            src="https://cdn-icons-png.flaticon.com/512/858/858133.png"
+            alt="bank image"
+          />
+          <div>
+            <p className="text-xl font-bold">Yes Bank</p>
+            <p className="text-xs ">*************1651</p>
+          </div>
+        </div>
+      </div>
+      <DialogClose className="w-full">
+        <Button
+          className="w-full py-7 text-xl"
+          onClick={handleSubmit}
+          variant="outline"
+        >
+          Withdraw
+        </Button>
+      </DialogClose>
+    </div>
+  );
 };
 
 export default WithdrawalForm;
