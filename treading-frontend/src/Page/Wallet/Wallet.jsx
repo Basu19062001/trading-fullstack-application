e9fae91308fce6f500b32fe5648fba4f4b26c81c
Basudev <samantabasu.2001@gmail.com>
2024-12-14 21:52:55 +0530

    Wallet section completed

--- treading-frontend/src/Page/Wallet/Wallet.jsx
+++ treading-frontend/src/Page/Wallet/Wallet.jsx
@@ -7,7 +7,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { ReloadIcon } from "@radix-ui/react-icons";
+import { ReloadIcon, UpdateIcon } from "@radix-ui/react-icons";
 import {
   ArrowDown,
   ArrowLeftCircle,
@@ -17,6 +17,7 @@ import {
   CreditCard,
   DollarSign,
   Shuffle,
+  ShuffleIcon,
   UploadIcon,
   WalletIcon,
 } from "lucide-react";
@@ -24,6 +25,7 @@ import React from "react";
 import TopupForm from "./TopupForm";
 import WithdrawalForm from "../Withdrawal/WithdrawalForm";
 import TransferForm from "./TransferForm";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 
 const Wallet = () => {
   return (
@@ -104,6 +106,35 @@ const Wallet = () => {
             </div>
           </CardContent>
         </Card>
+        <div className="py-5 pt-10">
+          <div className="flex gap-2 items-center pb-5">
+            <h1 className="text-2xl font-semibold">History</h1>
+            <UpdateIcon className="h-7 w-7 p-0 cursor-pointer hover:text-gray-400" />
+          </div>
+
+          <div className="space-y-5">
+            {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((item, i) => (
+              <div key={i}>
+                <Card className=" px-5 flex justify-between items-center">
+                  <div className="flex items-center gap-5">
+                    <Avatar>
+                      <AvatarFallback>
+                        <ShuffleIcon />
+                      </AvatarFallback>
+                    </Avatar>
+                    <div className="space-y-1">
+                      <h1>Buy Asset</h1>
+                      <p className="text-sm text-gray-500">2024-12-14</p>
+                    </div>
+                  </div>
+                  <div>
+                    <p className="text-green-500">999 USD</p>
+                  </div>
+                </Card>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
