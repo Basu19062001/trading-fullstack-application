d5da1cee7355d6a7c717ff70f75218d0f1a25d7a
Basudev <samantabasu.2001@gmail.com>
2024-12-14 23:34:34 +0530

    payment details completed

--- treading-frontend/src/Page/Payment Details/PaymentDetails.jsx
+++ treading-frontend/src/Page/Payment Details/PaymentDetails.jsx
@@ -21,36 +21,39 @@ const PaymentDetails = () => {
   return (
     <div className="px-20">
       <h1 className="text-3xl font-bold py-10">Payment Details</h1>
-      <Card>
-        <CardHeader>
-          <CardTitle>Yes Bank</CardTitle>
-          <CardDescription>A/C No : **********1651</CardDescription>
-        </CardHeader>
 
-        <CardContent>
-          <div className="flex items-center">
-            <p className="w-32">A/C Holder</p>
-            <p className="text-gray-400">Quester</p>
-          </div>
+      {false ? (
+        <Card>
+          <CardHeader>
+            <CardTitle>Yes Bank</CardTitle>
+            <CardDescription>A/C No : **********1651</CardDescription>
+          </CardHeader>
 
-          <div className="flex items-center">
-            <p className="w-32">IFSC</p>
-            <p className="text-gray-400">YESB0000007</p>
-          </div>
-        </CardContent>
-      </Card>
+          <CardContent>
+            <div className="flex items-center">
+              <p className="w-32">A/C Holder</p>
+              <p className="text-gray-400">Quester</p>
+            </div>
 
-      <Dialog>
-        <DialogTrigger>
-          <Button className="mt-5 py-6">Add payment datails</Button>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Payment Details</DialogTitle>
-          </DialogHeader>
-          <PaymentDetailsForm />
-        </DialogContent>
-      </Dialog>
+            <div className="flex items-center">
+              <p className="w-32">IFSC</p>
+              <p className="text-gray-400">YESB0000007</p>
+            </div>
+          </CardContent>
+        </Card>
+      ) : (
+        <Dialog>
+          <DialogTrigger>
+            <Button className="mt-5 py-6">Add payment datails</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Payment Details</DialogTitle>
+            </DialogHeader>
+            <PaymentDetailsForm />
+          </DialogContent>
+        </Dialog>
+      )}
     </div>
   );
 };
