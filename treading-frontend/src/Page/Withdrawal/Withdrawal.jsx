86e503f83ae82c503758d2ae632f25c7bddfbb73
Basudev <samantabasu.2001@gmail.com>
2024-12-14 23:36:56 +0530

    Withdrawal section started

--- treading-frontend/src/Page/Withdrawal/Withdrawal.jsx
+++ treading-frontend/src/Page/Withdrawal/Withdrawal.jsx
@@ -1,7 +1,12 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 
 const Withdrawal = () => {
-  return <div>Withdrawal</div>;
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Withdrawal;
